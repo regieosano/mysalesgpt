17f107384e25ea33a5352f64da459429b5821779
Reginald Osano <rgo02439@gmail.com>
2024-01-17 19:54:29 +0800

    Updated latest pushed

--- services/api/saleschat.py
+++ services/api/saleschat.py
@@ -6,8 +6,22 @@ from langchain.prompts import (
     MessagesPlaceholder,
 )
 from salesgpt.prompts import SALES_AGENT_ROLES
+from memory import conversation_memory
+from openai import chat
 
 
 class SalesChatService:
-	pass
+    def get_ai_chat_sales_answer(self, content: str):
+        chat_prompt = ChatPromptTemplate(
+            input_variables=["content", "messages"],
+            messages=[
+                MessagesPlaceholder(variable_name="messages"),
+                SystemMessagePromptTemplate.from_template(SALES_AGENT_ROLES),
+                HumanMessagePromptTemplate.from_template(content),
+            ],
+        )
 
+        chain = LLMChain(llm=chat, prompt=chat_prompt, memory=conversation_memory)
+
+        result = chain.invoke({"content": content})
+        return result["text"]
